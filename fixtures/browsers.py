0530b4ebc9dd831d87de25edaf3f81824cc674bc
danikshish <kinad910@gmail.com>
2026-08-19 20:08:26 +0300

    Запуск автотестов на CI

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -14,7 +14,7 @@ from tools.routes import AppRoute
 def page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
     yield from initialize_playwright_page(playwright, test_name=request.node.name, browser_type=request.param)
 
-@pytest.fixture(scope='session ')
+@pytest.fixture(scope='session')
 def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=settings.headless)
     context = browser.new_context(base_url=settings.get_base_url())
